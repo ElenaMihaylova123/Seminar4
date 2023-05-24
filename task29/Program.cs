20f09083fd2b9ce2173a6cddaf9afa07dfabd587
ElenaMihaylova <elena.16.04@mail.ru>
2023-05-24 23:02:48 +0700

    All done

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29.
+//Задание числами массива из 8 элементов и вывод массива
+
+
+int size = 8;
+int[] array = new int[8];
+Random myRandom = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = myRandom.Next(0, 20);
+    Console.Write("{0} ", array[i]);
+}
+Console.WriteLine("");
+Console.ReadKey();
